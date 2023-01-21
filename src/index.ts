1cd53e267c31152a3c87331c4a4c6a553c9a8cbc
Sam Parton <sam@idevelopthings.com>
2023-01-21 19:10:24 +0000

    Forgot to add exports

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export {Schema} from "./Schema";
 export {SchemaField} from "./SchemaField";
 export {SchemaTable} from "./SchemaTable";
 
+export {JsonSchema, type TableSchemaInfo} from "./JsonSchema";
+
 export {type TokenGroup, TokenProcessor} from "./Parser/TokenProcessor";
 export {SchemaParser, DefinedType} from "./Parser/SchemaParser";
 export {type IParser} from "./Parser/IParser";
